2c8be755fed19e5c2d0096833688e5b4faf1f258
OLeg <kvasnevskyi2019@gmail.com>
2023-03-30 21:45:29 +0200

    Initial commit

---
+++ src/main/java/com/educative/ecommerce/controller/CategoryController.java
@@ -0,0 +1,27 @@
+package com.educative.ecommerce.controller;
+
+import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/catgory")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public String createCategory(@RequestBody Category category) {
+        categoryService.createCategory(category);
+        return "success";
+    }
+
+    @GetMapping("/list")
+    public List<Category> listCategory() {
+        return categoryService.listCategory();
+    }
+}
